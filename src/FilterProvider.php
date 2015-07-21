0faafa26ed1e0a33aca1865f3ccd44c20fd53b6d
Akihito Koriyama <akihito.koriyama@gmail.com>
2015-07-21 13:04:45 +0900

    add AuraInputModule

---
+++ src/FilterProvider.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This file is part of the Ray.ValidateModule package
+ *
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ */
+namespace Ray\Validation;
+
+use Aura\Filter\FilterFactory;
+use Ray\Di\ProviderInterface;
+
+class FilterProvider implements ProviderInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function get()
+    {
+        return (new FilterFactory())->newFilter();
+    }
+}
